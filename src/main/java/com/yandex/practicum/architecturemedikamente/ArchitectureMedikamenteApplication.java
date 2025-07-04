9b2b3a932a72e3c666820a2c9330eea36ced3bd7
Mikhail Budkin <mbudkin@inno.tech>
2025-07-04 18:29:32 +0300

    begin

---
+++ src/main/java/com/yandex/practicum/architecturemedikamente/ArchitectureMedikamenteApplication.java
@@ -0,0 +1,12 @@
+package com.yandex.practicum.architecturemedikamente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitectureMedikamenteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchitectureMedikamenteApplication.class, args);
+    }
+}
